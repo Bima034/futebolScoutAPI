c8274c8d7a7195a6e5976d3fffdb95d67556007e
Anthonius <anthoniusmiguel@gmail.com>
2024-08-16 11:03:59 -0300

    adicionando campos no models.Clube

--- futebolScout/clube/models.py
+++ futebolScout/clube/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-
+from accounts.models import Pessoa
 # Create your models here.
 class Clube(models.Model):
     nome = models.CharField(max_length=100)
     sigla = models.CharField(max_length=3)
     pais = models.CharField(max_length=100)
     logo_path = models.CharField(max_length=250)
-    # treinador = 
-    # presidente =
+    treinador = models.ForeignKey(Pessoa)
+    presidente = models.ForeignKey(Pessoa)
     estadio = models.CharField(max_length=250)
     fundacao = models.DateField(auto_now=False, auto_now_add=False)
-    
+    jogadores =    
+
     def __str__(self):
         return self.nome
