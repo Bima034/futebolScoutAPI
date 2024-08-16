43887cd20e0d3c1c99f3e26b54583aead58afe91
Igor Souza Santos <igorsouza1771marketing@gmail.com>
2024-08-16 10:47:03 -0300

    criando app campeonato

--- futebolScout/futebolScout/settings.py
+++ futebolScout/futebolScout/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
+    'campeonato',
     #outrosApps
     'crispy_forms',
     'crispy_bootstrap5',
