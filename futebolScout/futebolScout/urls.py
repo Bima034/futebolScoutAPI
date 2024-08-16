f144c578c61b7bd8008c13b720f558e664577afa
Abimael Lima de Olivieri <abimael034@gmail.com>
2024-08-16 08:55:00 -0300

    INICIANDO APP JOGADOR E TEMPLATES

--- futebolScout/futebolScout/urls.py
+++ futebolScout/futebolScout/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('jogador/', include('jogador.urls')),
+    path('', include('jogador.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
