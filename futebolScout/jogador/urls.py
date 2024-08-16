ca7c1765620d0e7280dc6e46e3a6d56e754ade0a
Anthonius <anthoniusmiguel@gmail.com>
2024-08-16 15:57:49 -0300

    alterando jogador.urls e federacao.urls

--- futebolScout/jogador/urls.py
+++ futebolScout/jogador/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
-    path('list', views.listJogador, name='listJogador'),
-    path('detail', views.detailJogador, name='detailJogador'),
+    path('list/', views.listJogador, name='list-jogador'),
+    path('detail/<int:id_jogador>', views.detailJogador, name='detail-jogador'),
 ]
