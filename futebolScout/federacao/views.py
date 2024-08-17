242e2e7ec7097700820923bf84fb98a570bbfcf6
Igor Souza Santos <igorsouza1771marketing@gmail.com>
2024-08-17 17:27:49 -0300

    adicionando loguinrequired no settings

--- futebolScout/federacao/views.py
+++ futebolScout/federacao/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def detailFederacao(request):
+    return render(request, 'detailFederacao.html')
+
+def listFederacao(request):
+    return render(request, 'listFederacao.html')
