d7cae56943417786b92870c9ac4d050b75f1dd70
Anthonius <anthoniusmiguel@gmail.com>
2025-03-24 23:43:47 -0300

    :books: docs: create doc openswagger

--- futebolScout/futebolScout/urls.py
+++ futebolScout/futebolScout/urls.py
@@ -15,13 +15,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 #from jogador.views import dashboard
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FutebolScout API",
+        default_version='v1',
+        description="A API do FutebolScout fornece acesso a informações detalhadas sobre jogadores, clubes, campeonatos e federações. "
+                        "Ela permite a integração com sistemas externos para gerenciar e consultar dados relacionados ao futebol.",
+        terms_of_service="https://www.seusite.com/termos/",
+        contact=openapi.Contact(email="contato@seusite.com"),
+        license=openapi.License(name="Sua Licença"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),    path('admin/', admin.site.urls),
     #path('', dashboard, name='dashboard'),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
